4ae8619f0b616d0d1aa83c02bd39a3f26d8771e8
Titus Fortner <titusfortner@gmail.com>
2016-12-26 22:41:19 -0600

    fix build name for travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,8 @@ require "sauce_whisk"
 
 RSpec.configure do | config |
   config.before(:each) do | test |
-    build_name = ENV['JENKINS_BUILD_NUMBER'] ||
+    build_name = "Ruby-RSpec-Selenium-#{ENV['TRAVIS_JOB_NUMBER']}" ||
+        ENV['JENKINS_BUILD_NUMBER'] ||
         ENV['SAUCE_BAMBOO_BUILDNUMBER'] ||
         ENV['SAUCE_TC_BUILDNUMBER'] ||
         ENV['SAUCE_BUILD_NAME'] ||
