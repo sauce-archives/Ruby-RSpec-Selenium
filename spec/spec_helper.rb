7a0a33cca39ef10700c8b785e7061b9260f31224
Titus Fortner <titusfortner@gmail.com>
2016-12-26 23:18:29 -0600

    BUILD_TAG is responsibility of the CI Tool

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,24 +4,18 @@ require "sauce_whisk"
 
 RSpec.configure do | config |
   config.before(:each) do | test |
-    build_name = "Ruby-RSpec-Selenium-#{ENV['TRAVIS_JOB_NUMBER']}" ||
-        ENV['JENKINS_BUILD_NUMBER'] ||
-        ENV['SAUCE_BAMBOO_BUILDNUMBER'] ||
-        ENV['SAUCE_TC_BUILDNUMBER'] ||
-        ENV['SAUCE_BUILD_NAME'] ||
-        'LOCAL'
-
     capabilities = {
         version: ENV['version'],
         browserName: ENV['browserName'],
         platform: ENV['platform'],
         name: test.full_description,
-        build: build_name
+        build: ENV['BUILD_TAG'] || "Unknown Build"
     }
 
     url = "https://#{ENV['SAUCE_USERNAME']}:#{ENV['SAUCE_ACCESS_KEY']}@ondemand.saucelabs.com:443/wd/hub".strip
 
-    @browser = Watir::Browser.new(:remote, url: url, desired_capabilities: capabilities)
+    @driver = Selenium::WebDriver.for :remote, {url: url,
+                                                desired_capabilities: capabilities}
   end
 
   config.after(:each) do | example |
