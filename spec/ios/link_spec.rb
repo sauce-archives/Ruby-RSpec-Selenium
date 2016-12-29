6d0c55fa15f0aedfe474e9d7b58bf26cf3b3edd1
Titus Fortner <titusfortner@gmail.com>
2016-12-29 17:46:23 -0600

    add wait for potentially flaky test

--- spec/ios/link_spec.rb
+++ spec/ios/link_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe "Guinea Pig Link" do
   it "Verify Link opens page with correct title" do
     @driver.find_element(id: 'i am a link').click
-
+    Selenium::WebDriver::Wait.new.until { @driver.find_elements(id: 'i am a link').size == 0 }
     expect { @driver.find_element(id: 'i am a link') }.to raise_error Selenium::WebDriver::Error::NoSuchElementError
   end
 end
