d7273a8bf8a8edd177313db7a8d794fa4799700c
ndmanvar <ndmanvar@yahoo.com>
2017-05-25 17:42:32 -0600

    for cb

--- Rakefile
+++ Rakefile
@@ -1,5 +1,5 @@
 def run_tests(platform, browser, version, junit_dir)
-  system("platform=\"#{platform}\" browserName=\"#{browser}\" version=\"#{version}\" JUNIT_DIR=\"#{junit_dir}\" parallel_split_test spec")
+  system("PARALLEL_SPLIT_TEST_PROCESSES=100 platform=\"#{platform}\" browserName=\"#{browser}\" version=\"#{version}\" JUNIT_DIR=\"#{junit_dir}\" parallel_split_test spec")
 end
 
 
@@ -7,24 +7,9 @@ task :windows_10_chrome_58_1 do
   run_tests('Windows 10', 'chrome', '58', 'junit_reports/windows_10_chrome_58')
 end
 
-task :windows_10_chrome_58_2 do
-  run_tests('Windows 10', 'chrome', '58', 'junit_reports/windows_10_chrome_58')
-end
-
-task :windows_10_chrome_58_3 do
-  run_tests('Windows 10', 'chrome', '58', 'junit_reports/windows_10_chrome_58')
-end
-
-task :windows_10_chrome_58_4 do
-  run_tests('Windows 10', 'chrome', '58', 'junit_reports/windows_10_chrome_58')
-end
-
 
 multitask :test_sauce => [
-    :windows_10_chrome_58_1,
-    :windows_10_chrome_58_2,
-    :windows_10_chrome_58_3,
-    :windows_10_chrome_58_4
+    :windows_10_chrome_58_1
   ] do
     puts 'Running automation'
 end
--- spec/test1_spec.rb
+++ spec/test1_spec.rb
@@ -9,4 +9,460 @@ describe "Hudl repo" do
     @browser.get('http://www.hudl.com/home')
     @browser.save_screenshot("x.png")
   end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+  it "search text on google" do
+    @browser.manage.timeouts.implicit_wait = 5
+    @browser.manage.window.resize_to(1280, 720)
+    @browser.get('http://www.hudl.com/login')
+    @browser.manage.add_cookie(:name => 'CB_REPLACE', :value => "CB_REPLACE")
+    @browser.get('http://www.hudl.com/home')
+    @browser.save_screenshot("x.png")
+  end
+
+
+
+
+
+
 end
