d4dc432789a4f5f6b205eadf364acfbe772b5613
Titus Fortner <titusfortner@gmail.com>
2019-12-04 17:06:25 -0500

    implement desired simple sauce syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 require 'selenium-webdriver'
-require 'sauce_whisk'
+require 'simple_sauce'
+
 
 RSpec.configure do |config|
   config.before do |example|
@@ -13,35 +14,23 @@ RSpec.configure do |config|
   end
 
   def create_session(test_name)
-    # Sauce has multiple data centers, need to specify;
-    # Options are :US_WEST :EU_VDC and :US_EAST (for headless)
-    SauceWhisk.data_center = ENV['SAUCE_DC']&.to_sym || :US_WEST
+    platform[:name] = test_name
+    sauce_options = SimpleSauce::Options.new(platform)
 
-    # Ideal implementation is to set 'PLATFORM' environment variable in a Rake task, but we always include a defualt
-    options = platform(ENV['PLATFORM'] || 'mac_sierra_chrome')
-
-    options['sauce:options'] = {name: test_name,
-                                build: build_name,
-                                username: ENV['SAUCE_USERNAME'],
-                                access_key: ENV['SAUCE_ACCESS_KEY']}
-
-    # Make sure URL and Data Center value above match
-    url = ENV['SAUCE_URL'] || 'https://ondemand.saucelabs.com:443/wd/hub'
-
-    browser = options.delete(:browser_name).to_sym
-    capabilities = Selenium::WebDriver::Remote::Capabilities.send(browser, options)
-    @driver = Selenium::WebDriver.for(:remote, url: url,
-                                               desired_capabilities: capabilities)
+    @session = SimpleSauce::Session.new(sauce_options)
+    @session.data_center = (ENV['SAUCE_DC'] || 'US_WEST').to_sym
+    @driver = @session.start
   end
 
   def end_session(result)
-    SauceWhisk::Jobs.change_status(@driver.session_id, result)
-    @driver.quit
+    @session.stop(result)
   end
 
   # Ideal implementation is to do a lookup from a YAML file or the like rather than using a switch implementation
-  def platform(platform_key)
-    case platform_key
+  def platform
+    ENV['PLATFORM'] ||= 'mac_sierra_chrome'
+
+    case ENV['PLATFORM']
     when 'windows_10_edge'
       {platform_name: 'Windows 10',
        browser_name: 'edge',
@@ -65,20 +54,6 @@ RSpec.configure do |config|
     when 'headless'
       {platform_name: 'Linux',
        browser_name: 'chrome'}
-    end
-  end
-
-  #
-  # Note that this build name is specifically for Travis CI execution
-  # Most CI tools have ENV variables that can be structured to provide useful build names
-  #
-  def build_name
-    if ENV['TRAVIS_REPO_SLUG']
-      "#{ENV['TRAVIS_REPO_SLUG'][%r{[^/]+$}]}: #{ENV['TRAVIS_JOB_NUMBER']}"
-    elsif ENV['SAUCE_START_TIME']
-      ENV['SAUCE_START_TIME']
-    else
-      "Ruby-RSpec-Selenium: Local-#{Time.now.to_i}"
     end
   end
 end
